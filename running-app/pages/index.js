a7f3feec3025fc48ffe014baa95087542278f536
kensterlinggrow <ken@sterlinggrow.com>
2025-01-13 00:18:24 +0000

    update home page

---
+++ running-app/pages/index.js
@@ -0,0 +1,10 @@
+// pages/index.js
+
+export default function HomePage() {
+    return (
+      <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <h1>Welcome to My App</h1>
+        <p>This is a public page.</p>
+      </main>
+    );
+  }
